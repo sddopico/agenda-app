7d3e875f62446a83f468fc4ff3b66d0b1526dd94
Dominic Dopico <serafin.dopico@gmail.com>
2020-11-13 16:25:05 -0800

    add nav back 🙄

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
